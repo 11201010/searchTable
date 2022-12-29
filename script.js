bbac9660f3c96ff05331d65550fd3f63a18d22e1
11201010 <3a6l8j6l@gmail.com>
2022-12-30 02:31:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const resultTemplate = document.getElementsByTagName("template")[0];
 const tbody = document.getElementsByTagName("tbody")[0];
 const searchInput = document.getElementById("searchInput");
 
+let products = [];
+
 searchInput.addEventListener("input", (event) => {
-	const value = event.target.value;
-	console.log(value);
+	const value = event.target.value.toLowerCase();
+	products.forEach((product) => {
+		const isVisible =
+			product.id.toLowerCase().includes(value) ||
+			product.name.toLowerCase().includes(value) ||
+			product.address.toLowerCase().includes(value);
+		product.element.classList.toggle("hidden", !isVisible);
+	});
 });
 
 fetch("./products.json")
 	.then((response) => response.json())
 	.then((data) => {
-		data.products.forEach((product) => {
+		products = data.products.map((product) => {
 			const result = resultTemplate.content.cloneNode(true).children[0];
 			const resultId = result.querySelector("[data-id]");
 			const resultName = result.querySelector("[data-name]");
@@ -19,5 +27,11 @@ fetch("./products.json")
 			resultName.textContent = product.name;
 			resultAddress.textContent = product.address;
 			tbody.append(result);
+			return {
+				id: product.id,
+				name: product.name,
+				address: product.address,
+				element: result,
+			};
 		});
 	});
