c5caaebf27608fe854a28032aa7eafca71115801
11201010 <3a6l8j6l@gmail.com>
2022-12-30 01:55:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const resultTemplate = document.getElementsByTagName("template")[0];
+const tbody = document.getElementsByTagName("tbody")[0];
+
 fetch("./products.json")
 	.then((response) => response.json())
 	.then((data) => {
-		const result = resultTemplate.content.cloneNode(true).children[0];
-		console.log(result);
+		data.products.forEach((product) => {
+			const result = resultTemplate.content.cloneNode(true).children[0];
+			const resultId = result.querySelector("[data-id]");
+			const resultName = result.querySelector("[data-name]");
+			const resultAddress = result.querySelector("[data-address]");
+			resultId.textContent = product.id;
+			resultName.textContent = product.name;
+			resultAddress.textContent = product.address;
+			tbody.append(result);
+		});
 	});
