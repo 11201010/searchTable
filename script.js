724bfc898a83fbb2f78de358270782409884b129
11201010 <3a6l8j6l@gmail.com>
2022-12-30 01:40:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const resultTemplate = document.getElementsByTagName("template")[0];
 fetch("./products.json")
 	.then((response) => response.json())
-	.then((data) => console.log(data));
+	.then((data) => {
+		const result = resultTemplate.content.cloneNode(true).children[0];
+		console.log(result);
+	});
